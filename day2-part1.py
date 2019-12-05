a2b7bb43990bac3cf99306e7d82d64467fb393e2
Daphné Lesur <daphne.lesur@gmail.com>
2019-12-05 14:01:15 +0100

    Program

---
+++ day2-part1.py
@@ -0,0 +1,27 @@
+# Day 2 - Part 1
+donnees=input()
+classe=donnees.split(",")
+nb=0
+x=0
+
+
+# 1202 program alarm
+classe[1]=12
+classe[2]=2
+print(classe)
+
+
+def op (nb1, nb2, nb3, nb4):
+    if nb1==1:
+        classe[nb4]=int(classe[nb2])+int(classe[nb3])
+    else:
+        classe[nb4]=int(classe[nb2])*int(classe[nb3])
+
+
+while classe[x] != "99":
+    op(int(classe[x]), int(classe[x+1]), int(classe[x+2]), int(classe[x+3]))
+    x=x+4
+
+
+print(classe)
+print("here :", classe[0])
