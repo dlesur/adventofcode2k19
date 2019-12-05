307b10bc4907ce4664d6748432d9581e8b8b3a3a
Daphné Lesur <daphne.lesur@gmail.com>
2019-12-05 12:57:41 +0100

    Program

--- day1-part1.py
+++ day1-part1.py
@@ -1,1 +1,22 @@
 # Day 1 - Part 1
+from math import*
+
+
+source=open("/home/daphne/Documents/python/puzzle-input.csv", "r")
+lines =source.readlines()
+source.close
+donnees=[0]*len(lines)
+x=0
+for line in lines:
+    donnees[x]=line.rstrip()
+    x=x+1
+
+
+total=0
+
+for loop in range(len(donnees)):
+    a=(floor(float(donnees[loop])/3)-2) 
+    total=total+a
+    b=a
+
+print("here:", total)
