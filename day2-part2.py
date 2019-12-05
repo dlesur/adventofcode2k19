1a375b8ea1a42e5b44519f19e3422e9b93cb1f9b
Daphné Lesur <daphne.lesur@gmail.com>
2019-12-05 15:29:57 +0100

    program

--- day2-part2.py
+++ day2-part2.py
@@ -1,8 +1,8 @@
 # Day 2 - Part 2
 donnees=input()
-classe=donnees.split(",")
-nb=0
-x=0
+soluce=0
+nbtest=0
+nbtest2=0
 
 
 # 1202 program alarm
@@ -17,10 +17,22 @@ def op (nb1, nb2, nb3, nb4):
         classe[nb4]=int(classe[nb2])*int(classe[nb3])
 
 
-while classe[x] != "99":
-    op(int(classe[x]), int(classe[x+1]), int(classe[x+2]), int(classe[x+3]))
-    x=x+4
+while soluce!=19690720:
+    x=0
+    classe=donnees.split(",")
+    classe[1]=nbtest
+    classe[2]=nbtest2
+    while classe[x] != "99":
+        op(int(classe[x]), int(classe[x+1]), int(classe[x+2]), int(classe[x+3]))
+        x=x+4
+    if nbtest==99:
+        nbtest2=nbtest2+1
+        nbtest=0
+    else:
+        nbtest=nbtest+1
+    soluce=classe[0]
 
 
-print(classe)
-print("here :", classe[0])
+print(classe[0])
+print("here :", classe[1], "and :", classe[2])
+print("soluce :", classe[1]*100 + classe[2])
